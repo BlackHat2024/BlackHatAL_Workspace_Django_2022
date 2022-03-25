f1b304343f686ab4305e0d9ac5263aa81218bde9
BlackHat2024 <flaviopernoj2024@gmail.com>
2022-03-25 13:15:12 +0100

    Update

--- Start/Prova1/views.py
+++ Start/Prova1/views.py
@@ -1,9 +1,11 @@
 from cgitb import html
 from django.shortcuts import render
 from django.http import HttpResponse
+from Prova1.models import AccessRecord,Webpage,Topic
 
 # Create your views here.
 
 def index(request):
-    a={'insertme':"view.py"}
-    return render(request,'Prova1/index.html',context=a)
+    webpage_list = AccessRecord.objects.order_by('date')
+    dd = {'ar':webpage_list}
+    return render(request,'Prova1/index.html',context=dd)
